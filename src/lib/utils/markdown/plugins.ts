c27d6fd62110d2d832d8754c9b544acc485ea0a2
w3cj <cj@null.computer>
2024-08-19 14:34:56 -0600

    add markdown editor

---
+++ src/lib/utils/markdown/plugins.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import toc from 'markdown-it-toc';
+import footnote from 'markdown-it-footnote';
+import sub from 'markdown-it-sub';
+import sup from 'markdown-it-sup';
+import mark from 'markdown-it-mark';
+import deflist from 'markdown-it-deflist';
+import ins from 'markdown-it-ins';
+import abbr from 'markdown-it-abbr';
+import checkbox from 'markdown-it-checkbox';
+
+export default [toc, footnote, sub, sup, mark, deflist, ins, abbr, checkbox];
--- src/routes/project/[id]/+page.svelte
+++ src/routes/project/[id]/+page.svelte
@@ -1,20 +1,98 @@
 <script lang="ts">
 	import { app_data } from '$state/Project.svelte.js';
+	import marked from '$lib/utils/markdown';
+	import { ink, defineOptions } from 'ink-mde';
+	import { onMount } from 'svelte';
 	let { data } = $props();
+
+	let editor: HTMLDivElement | null = null;
+	let notes = app_data.project.notes || '';
+
+	// Syntax highlighting can be customized with CSS variables
+	// https://github.com/davidmyersdev/ink-mde/tree/main?tab=readme-ov-file#syntax-highlighting
+	const options = defineOptions({
+		doc: notes,
+		interface: {
+			toolbar: true,
+			appearance: 'dark',
+			attribution: false
+		},
+		hooks: {
+			afterUpdate(doc) {
+				notes = doc;
+				app_data.save({ id: data.id, notes });
+			}
+		}
+	});
+	onMount(() => {
+		if (editor) {
+			ink(editor, options);
+		}
+	});
+
+	const copyToClipboard = async (text: string) => {
+		const type = 'text/plain';
+		const blob = new Blob([text], { type });
+		const data = [new ClipboardItem({ [type]: blob })];
+		await navigator.clipboard.write(data);
+	};
+
+	const copyHtml = () => {
+		const html = marked.render(notes);
+		copyToClipboard(html);
+	};
+
+	const copyText = () => {
+		const html = marked.render(notes);
+		const element = document.createElement('div');
+		element.innerHTML = html;
+		const textNodes: string[] = [];
+		element.childNodes.forEach((node) => {
+			textNodes.push(node.textContent || '');
+		});
+
+		const text = textNodes.join('\n');
+		copyToClipboard(text);
+	};
 </script>
 
-<h1
-	oninput={(e) => {
-		app_data.save({ id: data.id, name: e.target.innerHTML });
-	}}
-	contenteditable
->
-	{app_data.project.name}
-</h1>
+<div class="container">
+	<h1
+		oninput={(e) => {
+			app_data.save({ id: data.id, name: e.target.textContent });
+		}}
+		contenteditable
+	>
+		{app_data.project.name}
+	</h1>
+	<div>
+		<button onclick={copyHtml}>Copy as HTML</button>
+		<button onclick={copyText}>Copy as Text</button>
+		<button onclick={() => copyToClipboard(notes)}>Copy as Markdown</button>
+	</div>
+	<div class="editor" bind:this={editor}></div>
+</div>
 
 <style>
 	h1:focus {
 		outline: none;
 		border-bottom: solid 1px var(--yellow);
+	}
+
+	.container {
+		height: 90vh;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+
+	.editor {
+		flex-grow: 1;
+		overflow: hidden;
+
+		:global(.ink),
+		:global(.cm-editor) {
+			height: 100%;
+		}
 	}
 </style>
